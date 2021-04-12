8e8dede4e1441206b086c0d5c7ce3f843785ca08
dobaradar <rezamansourikhah@gmail.com>
2021-04-12 18:47:54 +0430

    add 2 models for connection

--- models/user.go
+++ models/user.go
@@ -1,9 +1,9 @@
 package models
 
 type NewUser struct {
-	Username  string `json:"username"`
-	Email     string `json:"email"`
-	Password  string `json:"password"`
+	Username string `json:"username"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
 }
 
 type RefreshToken struct {
@@ -11,11 +11,24 @@ type RefreshToken struct {
 }
 
 type UserInfo struct {
-	Email     string `json:"email"`
-	Username  string `json:"username"`
+	Email    string `json:"email"`
+	Username string `json:"username"`
 }
 
 type UserCredential struct {
 	Username string `json:"username"`
 	Password string `json:"password"`
+}
+
+type UserAddToken struct {
+	Token      string `json:"token"`
+	Token_type string `json:"token_type"`
+	UserID     uint   `json:"userid"`
+	Name       string `json:"name"`
+}
+type UserGetToken struct {
+	Token_type string `json:"token_type"`
+	UserID     uint   `json:"userid"`
+	Name       string `json:"name"`
+	Token_id   uint   `json:"id"`
 }
