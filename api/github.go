5191c8126df43023581b3c7da9b32e2426e9dd06
rezamnkh79 <rezamansourikhah@gmail.com>
2021-05-29 23:55:26 +0430

    change field name

--- api/github.go
+++ api/github.go
@@ -6,6 +6,6 @@ type GitInfo struct {
 	Id               uint          `json:"id"`
 	GitUsername      string        `json:"username"`
 	Token            *oauth2.Token `json:"token"`
-	ConnectionID     uint          `json:"connectionid"`
-	RepositoriesName string        `json:"repository_name"`
+	ConnectionID     uint          `json:"connectionId"`
+	RepositoriesName string        `json:"repositoryName"`
 }
