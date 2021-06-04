d4c3cd1ec8d947e41174505d99c012e8ea7b9ff3
Amin-MAG <M.Amin.Ghasvari@gmail.com>
2021-06-04 21:09:44 +0430

    Add author to response

--- api/discord.go
+++ api/discord.go
@@ -118,6 +118,10 @@ type Message struct {
 	// The roles mentioned in the message.
 	MentionRoles []string `json:"mention_roles"`
 
+	// The author of the message. This is not guaranteed to be a
+	// valid user (webhook-sent messages do not possess a full author).
+	Author *DiscordUser `json:"author"`
+
 	// Whether the message is text-to-speech.
 	TTS bool `json:"tts"`
 
@@ -142,3 +146,51 @@ type Message struct {
 }
 
 type Messages []Message
+
+
+// A DiscordUser stores all data for an individual Discord user.
+type DiscordUser struct {
+	// The ID of the user.
+	ID string `json:"id"`
+
+	// The email of the user. This is only present when
+	// the application possesses the email scope for the user.
+	Email string `json:"email"`
+
+	// The user's username.
+	Username string `json:"username"`
+
+	// The hash of the user's avatar. Use Session.UserAvatar
+	// to retrieve the avatar itself.
+	Avatar string `json:"avatar"`
+
+	// The user's chosen language option.
+	Locale string `json:"locale"`
+
+	// The discriminator of the user (4 numbers after name).
+	Discriminator string `json:"discriminator"`
+
+	// The token of the user. This is only present for
+	// the user represented by the current session.
+	Token string `json:"token"`
+
+	// Whether the user's email is verified.
+	Verified bool `json:"verified"`
+
+	// Whether the user has multi-factor authentication enabled.
+	MFAEnabled bool `json:"mfa_enabled"`
+
+	// Whether the user is a bot.
+	Bot bool `json:"bot"`
+
+	// The type of Nitro subscription on a user's account.
+	// Only available when the request is authorized via a Bearer token.
+	PremiumType int `json:"premium_type"`
+
+	// Whether the user is an Official Discord System user (part of the urgent message system).
+	System bool `json:"system"`
+
+	// The flags on a user's account.
+	// Only available when the request is authorized via a Bearer token.
+	Flags int `json:"flags"`
+}
